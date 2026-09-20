79f58e3c28877bee4a9a1e5df24ed80c78348903
Shikochin <i@shikoch.in>
2026-09-20 12:59:30 +0800

    chore: bump version to 1.2.7.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
         minSdk = 29
         targetSdk = 37
         versionCode = maimaidBuildNumber.get()
-		    versionName = "1.2.7"
+		    versionName = "1.2.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "BACKEND_URL", "\"${maimaidBackendUrl.get()}\"")
